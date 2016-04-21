6b88a659db241ef577d607a247016c1b2a2a9eb7
otrofama <otrofama@gmail.com>
2016-04-20 19:50:17 -0500

    Files parser, lex

---
+++ parser.c
@@ -0,0 +1,362 @@
+#include <token.h>
+extern LIST* symbols
+
+
+int token;
+
+
+void error()
+{
+	printf("SYNTAX ERROR\n");
+}
+void eat(int tok)
+{
+	if (token==tok)
+		token=yylex();
+	else
+		error();//SYNTAX ERROR
+}
+
+
+//Segun diagramas de sintaxis
+void P()
+{
+	D();
+	R();
+}
+
+void R()
+{
+	F();
+	RP();
+}
+void F()
+{
+	eat(FUNC);
+	T();
+	eat(ID);
+	eat(ARG);
+	eat(PAR_AB);
+	G();
+	eat(PAR_CI);
+	eat(LLA_AB);
+	B();
+	eat(LLA_CI);
+}
+
+void RP()
+{
+	while(token==FUNC)
+	T();
+}
+
+void D()
+{
+	
+}
+void DP()
+{
+	while(token==var)
+	{
+		eat(var);
+		T();
+		L();
+		eat(PCOM);
+	}
+}
+
+void L()
+{
+	I();
+	LP();
+}
+
+void I()
+{
+	eat(ID);
+	IP();
+}
+void V()
+{
+	switch(token)
+	{
+		case NUM:
+			eat(NUM);
+			break;
+		case CARACTERES:
+			eat(CARACTERES);
+			break;
+		case ID:
+			eat(ID);
+			break;
+		default:
+			error();
+	}
+}
+
+void T()
+{
+	switch(token)
+	{
+		case INT:
+			eat(INT);
+			break;
+		case FLOAT:
+			eat(FLOAT);
+			break;
+		case CHAR:
+			eat(CHAR);
+			break;
+		default:
+			error();
+	}
+}
+void GP()
+{
+	while(token==ID)
+	{
+		eat(ID);
+		T();
+		eat(COMA);
+	}
+}
+
+void X()
+{
+	eat(while);
+	eat(PAR_AB);
+	U();
+	eat(PAR_CI);
+	eat(LLA_AB);
+	B();
+	eat(LLA_CI);
+}
+
+void M()
+{
+	eat(FOR);
+	eat(PAR_AB);
+	A();
+	eat(PCOM);
+	U();
+	eat(PCOM);
+	O();
+	eat(PAR_CI);
+	eat(LLA_AB);
+	B();
+	eat(LLA_CI);
+}
+
+void Z()
+{
+	eat(IF);
+	eat(PAR_AB);
+	U();
+	eat(PAR_CI);
+	eat(LLA_AB);
+	B();
+	eat(LLA_CI);
+	ZP();
+}
+void ZP()
+{//epsilon
+	eat(ELSE);
+	eat(LLA_AB);
+	B();
+	eat(LLA_CI);
+}
+
+void S()
+{
+	eat(SWITCH);
+	eat(PAR_AB);
+	eat(ID);
+	eat(PAR_CI);
+	eat(LLA_AB);
+	N();
+	eat(LLA_CI);
+}
+
+void N()
+{
+	switch(token)
+	{
+		case CASE:
+			eat(CASE);
+			V();
+			eat(PCOM);
+			B();
+			Q();
+			N();
+			break;
+		case DEFAULT:
+			eat(DEFAULT);
+			eat(PCOM);
+			B();
+			Q();
+			break;
+		default:
+			error();
+	}
+}
+
+void E()
+{
+	E1();
+	E2();
+}
+void E1()
+{
+	E3();
+	E4();
+}
+void E2()
+{
+
+}
+void E3()
+{
+	switch(token)
+	{
+		case PAR_AB:
+			eat(PAR_AB);
+			E();
+			eat(PAR_CI);
+			break;
+		case ID:
+			eat(ID);
+			break;
+		case CARACTERES:
+			eat(CARACTERES);
+			break;
+		//falta case A();
+	}
+}
+void E4()
+{
+
+}
+
+void J()
+{
+	K();
+	JP();
+}
+
+void JP()
+{
+	while(token==WHILE || token==FOR ||token==PAR_AB||token==ID
+		||token==CARACTERES||token==IF||token==SWITCH)
+	{
+		if(token==WHILE)
+	}
+}
+
+void B()
+{
+
+}
+
+void A()
+{
+	eat(ID);
+	eat(IGUAL);
+	E();
+}
+
+void O()
+{
+	eat(ID);
+	OP();
+}
+void OP()
+{
+	switch(token)
+	{
+		case DMAS:
+			eat(DMAS);
+			break;
+		case DMENOS:
+			eat(DMENOS);
+			break;
+		default:
+			error();
+	}
+}
+
+void Q()
+{
+
+}
+
+void U()
+{
+	U1();
+	U5();
+}
+void U1()
+{
+	U2();
+	U6();
+}
+void U2()
+{
+	U3();
+	U7();
+}
+void U3()
+{
+	U4();
+	U8();
+}
+void U4()
+{
+	switch(token)
+	{
+		case _NOT:
+			eat(_NOT);
+			U4();
+			break;
+		case //E:
+	}
+}
+void U5()
+{
+	while(token=_OR)
+	{
+		eat(_OR);
+		U1();
+	}
+}
+void U6()
+{
+	while(token==_AND)
+	{
+		eat(_AND);
+		U2();
+	}
+}
+
+void U7()
+{
+
+}
+void U8()
+{
+
+}
+
+void init()
+{
+	symbols=(LIST*)malloc(sizeof(LIST));
+	//insertar simbolos
+}
+
+int main(int argc, char const *argv[])
+{
+	//abrir archivo...
+	token=yylex();
+	init();
+	P();		//simbolo inicial..
+	return 0;
+}
---
+++ token.h
@@ -0,0 +1,66 @@
+/*git gui
+git config --global user.name <name>
+git config --global user.mail <mail>*/
+
+
+#define INT		1
+#define FLOAT	2
+#define CHAR	3
+#define VAR 	4
+#define FUNC	5
+#define ARG		6
+#define WHILE	7
+#define IF		8
+#define ELSE	9
+#define SWITCH	10
+#define CASE	11
+#define FOR		13
+#define ID		14
+#define ENTEROS	15
+#define FLOTANTES		16
+#define CARACTERES		17
+#define RETURN		18
+#define DEFAULT		19
+#define BREAK		20
+
+#define DP			21 //:
+#define PCOM		22 //;
+#define COMA		23 //,
+#define PAR_AB		24 //(
+#define PAR_CI		25 //)
+#define LLA_AB		26 //{
+#define LLA_CI		27 //}
+
+#define MAS			28 //+
+#define MENOS		29 //-
+#define POR			30 //*
+#define DIV			31 ///
+#define MOD			32 //%
+#define PIC_CI		33 //>
+#define PIC_AB		34 //<
+#define MAY_IG		35 //>=
+#define MEN_IG		36 //<=
+#define DOB_IG		37 //==
+#define DIF			38 //<>
+#define _AND		39 //&&
+#define _OR			40 //||
+#define _NOT		41 //!
+#define DMAS		43 //++
+#define DMENOS		44 //--
+#define COMMENT		45 //--
+#define IGUAL		46 //=
+
+
+typedef union YYSTYPE YYSTYPE;
+
+union YYSTYPE
+{
+	char* sval;
+	float fval;
+	int ival;
+	char cval;//caracteres
+	int line;
+	int column;
+};
+YYSTYPE yylval;
+
